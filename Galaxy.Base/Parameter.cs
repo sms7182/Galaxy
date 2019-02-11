a937aa7cd96cc13405985111fb60abda9993ebad
sms7182 <sms7182@gmail.com>
2019-02-11 07:29:37 +0330

    change name of class

--- Galaxy.Base/Parameter.cs
+++ Galaxy.Base/Parameter.cs
@@ -5,7 +5,7 @@ using System.Text;
 namespace Galaxy.Base
 {
 
-    public class Feature:BusinessObject
+    public class Parameter:BusinessObject
     {
         public virtual int Value { get; set; }
         public virtual string Name { get; set; }
